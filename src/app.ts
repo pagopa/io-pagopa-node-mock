cba1fe2552912ccb22e56761052264089ffefb90
pasqualespica <pasqualespica@gmail.com>
2021-08-17 10:56:10 +0200

    [PAG-606] add log RX requests

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,8 @@ export async function newExpressApp(
   // SOAP Server mock entrypoint
   app.post(config.NODO_MOCK.ROUTES.PPT_NODO, async (req, res) => {
     const soapRequest = req.body["soap:envelope"]["soap:body"][0];
-    
+    logger.info("Rx request : ")
+    logger.info(soapRequest)
     // The SOAP request is a NodoAttivaRPT request
     if (soapRequest["ppt:nodoattivarpt"]) {
       const nodoAttivaRPT = soapRequest["ppt:nodoattivarpt"][0];
