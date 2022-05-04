c0562f0336ef9deca324ed775cf5b6145ac8088e
Simone Infante <simone.infante@pagopa.it>
2022-05-04 18:26:52 +0200

    te

--- src/fixtures/nodoRPTResponses.ts
+++ src/fixtures/nodoRPTResponses.ts
@@ -136,14 +136,14 @@ export const activatePaymenNoticeResponse = (
   200,
   `<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:ns="http://pagopa-api.pagopa.gov.it/node/nodeForIO.xsd">
    <soapenv:Body>
-    <activatePaymentNoticeRes>
+    <ns:activatePaymentNoticeRes>
      <outcome>OK</outcome>
      <totalAmount>100.00</totalAmount>
      <paymentDescription>Quota Albo Ordine Giornalisti 2022</paymentDescription>
      <fiscalCodePA>80017010721</fiscalCodePA>
      <paymentToken>4d500e9908714202b55751ed6b1dfdf3</paymentToken>
      <creditorReferenceId>01225090200079679</creditorReferenceId>
-    </activatePaymentNoticeRes>
+    </ns:activatePaymentNoticeRes>
    </soapenv:Body>
   </soapenv:Envelope>
  `
